1fab6f93771e09f22dcd9f9e93905577469813b1
lonsdale341 <lonsdale341@gmail.com>
2018-10-04 03:46:30 +0300

    fix 1

--- Assets/SamplesResources/SceneAssets/GroundPlane/Scripts/PlaneManager.cs
+++ Assets/SamplesResources/SceneAssets/GroundPlane/Scripts/PlaneManager.cs
@@ -168,7 +168,7 @@ public class PlaneManager : MonoBehaviour
                    // CommonData.currentUser.data.Plus += 300;
                    // CommonData.currentUser.data.Minus -= 200;
                     CommonData.mainManager.stateManager.PushState(new ShowTitle());
-                    
+                    CommonData.prefabs.gameobjectLookup[StringConstants.ARCamera].GetComponent<AudioSource>().Play();
                    // CommonData.StateAnimal = true;
                     setOblect = true;
                     break;
